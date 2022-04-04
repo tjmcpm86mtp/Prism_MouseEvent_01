35b3c480b5d88bce2103a973669b5cd50504d6a8
tjmcpm86mtp <tjmcpm86mtp@outlook.jp>
2022-04-05 08:55:02 +0900

    クラスを追加

---
+++ BlankCoreApp_2019Original/Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prism_MouseEvent_01.Models
+{
+    internal class Class1
+    {
+    }
+}
